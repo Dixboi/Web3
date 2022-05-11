4fbfe6a4dacc38ba0d86ba934be86a588338913f
Dex <59426458+Dixboi@users.noreply.github.com>
2022-05-11 16:19:15 +0800

    Update SolidityPath_Lesson.sol

--- CryptoZombies/SolidityPath_Lesson.sol
+++ CryptoZombies/SolidityPath_Lesson.sol
@@ -1,5 +1,3 @@
-pragma solidity >=0.5.0 <0.6.0; //always put version
-
 /* A contract for hello world
 contract HelloWorld {
 
@@ -37,12 +35,26 @@ view -- returns a value within the app
 pure -- returns a value based from parameters
 */
 
-contract ZombieFactory { 
+/* declare the event
+event IntegersAdded(uint x, uint y, uint result);
+
+function add(uint _x, uint _y) public returns (uint) {
+  uint result = _x + _y;
+  // fire an event to let the app know the function was called:
+  emit IntegersAdded(_x, _y, result);
+  return result;
+}
+*/
+
+pragma solidity >=0.5.0 <0.6.0; //always put version
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-    
-    // struct is like an obejct with multiple attributes
+
     struct Zombie {
         string name;
         uint dna;
@@ -50,12 +62,20 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private{
-        zombies.push(Zombie(_name, _dna));
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
+
