3c8e2884528a991e93250c1c6490deb465afa15b
Dex <59426458+Dixboi@users.noreply.github.com>
2022-05-11 14:25:48 +0800

    Update Lesson1_notes.sol

--- CryptoZombies/Lesson1_notes.sol
+++ CryptoZombies/Lesson1_notes.sol
@@ -23,6 +23,20 @@ function eatHamburgers(string memory _name, uint _amount) public {
 }
 */
 
+//it's convention to start private function names with an underscore (_)
+
+/*
+Returning values in a function
+string greeting = "What's up dog";
+
+function sayHello() public returns (string memory) {
+  return greeting;
+}
+
+view -- returns a value within the app
+pure -- returns a value based from parameters
+*/
+
 contract ZombieFactory { 
 
     uint dnaDigits = 16;
@@ -36,10 +50,12 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string memory _name, uint _dna) public{
-
+    function _createZombie(string memory _name, uint _dna) private{
+        zombies.push(Zombie(_name, _dna));
     }
 
-    
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+
+    }
 
 }
