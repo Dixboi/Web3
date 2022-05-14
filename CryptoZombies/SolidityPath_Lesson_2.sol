f57cd054de9a2a27469cbd39652dfdebcb6e7415
Dex <59426458+Dixboi@users.noreply.github.com>
2022-05-14 10:35:44 +0800

    Update SolidityPath_Lesson_2.sol

--- CryptoZombies/SolidityPath_Lesson_2.sol
+++ CryptoZombies/SolidityPath_Lesson_2.sol
@@ -1,12 +1,11 @@
 /* Defining a mapping
-
 // For a financial app, storing a uint that holds the user's account balance:
 mapping (address => uint) public accountBalance;
 // Or could be used to store / lookup usernames based on userId
 mapping (uint => string) userIdToName;
-
 */
 
+// FILE NAME IS zombiefactory.sol
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
@@ -26,8 +25,10 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     } 
 
@@ -37,8 +38,81 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
 }
+
+//FILE NAME IS zombiefeeding.sol
+//Inheritance
+pragma solidity >=0.5.0 <0.6.0;
+
+/*
+Storage - variables stored permanently on the blockchain
+Memory - variables that are temporary
+*/
+
+import "./zombiefactory.sol";
+
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+    );
+}
+
+contract ZombieFeeding is ZombieFactory {
+
+    
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+        newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+  }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+      uint kittyDna;
+      (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+      feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
+}
+
+/*
+function multipleReturns() internal returns(uint a, uint b, uint c) {
+  return (1, 2, 3);
+}
+
+function processMultipleReturns() external {
+  uint a;
+  uint b;
+  uint c;
+  // This is how you do multiple assignment:
+  (a, b, c) = multipleReturns();
+}
+
+// Or if we only cared about one of the values:
+function getLastReturnValue() external {
+  uint c;
+  // We can just leave the other fields blank:
+  (,,c) = multipleReturns();
+}
+*/
